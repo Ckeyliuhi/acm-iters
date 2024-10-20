f1c28d45b6cd09db9d7dcefcb5dce7aa7cf436ee
iters <149059634+Ckeyliuhi@users.noreply.github.com>
2024-10-20 10:59:49 +0800

    Update prime.cpp

--- 数学/素数/prime.cpp
+++ 数学/素数/prime.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-namespace Prime {
+using i64 = int64_t;
+    
+namespace Prime { 
     constexpr int N = 1e5 + 5;
-    bool isPrime (int x) { // √x
+    bitset <N> sieve_prime;
+    void seive (int n = N - 1) { // sieve (n log n)
+        sieve_prime[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (!sieve_prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    sieve_prime[j] = 1;
+                }
+            }
+        }
+    } 
+    bool is_prime (int x) { // √x
         if (x < 2) return false;
         for (int i = 2; i <= x / i; i++) {
             if (x % i == 0) {
@@ -13,65 +25,49 @@ namespace Prime {
         }
         return true;
     }
-
-    vector <int> getFactors (int n) { // √x 非顺序 
+    vector <int> getFactor (int x) { // √x
         vector <int> factors;
-        // set <int> factors;
-        for (int i = 1; i <= n / i; i++) {
-            if (n % i == 0) {
+        for (int i = 1; i <= x / i; i++) {
+            if (x % i == 0) {
                 factors.push_back(i);
-                if (i * i != n) {
-                    factors.push_back(n / i);
+                if (i != x / i) {
+                    factors.push_back(x / i);
                 }
-                // factors.insert(i);
-                // factors.insert(n / i);
             }
         }
         return factors;
-        // return vector <int> (factors.begin(), factors.end());
     }
-
-    bitset <N> sieve_prime;  
-    
-    void sieve (int n = N) { // N log log N
-        sieve_prime[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (!sieve_prime[i]) {
-                for (int j = i + i; j <= n; j += i) {
-                    sieve_prime[j] = 1;
-                }
-            }
-        }   
-    }
-
-    bitset <N> eluer_prime;
+    bitset <N> euler_prime;
     vector <int> prime;
-    int pre_prime[N];
-    void eluer (int n = N) { // N 
+    int prime_cnt[N] {};
+    void euler (int n = N - 1) {
         for (int i = 2; i <= n; i++) {
-            pre_prime[i] = pre_prime[i - 1];
-            if (!eluer_prime[i]) {
-                prime.push_back(i);
-                pre_prime[i] ++;   
-            } 
-            for (int& p : prime) {
-                if (i * p >= n) break;
-                eluer_prime[i * p] = 1;
+            prime_cnt[i] = prime_cnt[i - 1];
+            if (!euler_prime[i]) {
+                prime.emplace_back(i);
+                prime_cnt[i]++;
+            }
+            for (int &p : prime) {
+                if (p * i >= n) break;
+                euler_prime[p * i] = 1;
                 if (i % p == 0) break;
             }
         }
-    } 
+    }
 } using namespace Prime;
 
 void solve() {
-    
+    euler();
+    for (int i : prime) {
+        cout << i << ' ';
+    }
 }
 
 int main() {
     cin.tie(0) -> sync_with_stdio(false);
-    // freopen("C:/Users/26842/Desktop/acm/in.txt", "r", stdin);
-    int _ = 1;  
-    while (_--) {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
         solve();
     }
     return 0;
